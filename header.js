73cd394b5ede3c7886ed103f67ddd8b1b56c9643
hainazheng <hainazheng1991@outlook.com>
2022-07-06 16:54:28 +0200

    added note to js file
    i added today's note to the js file i created

--- header.js
+++ header.js
@@ -1,1 +1,25 @@
 // added header
+
+
+//github, gitlab, gitbucket 
+
+//1. Create: create a git repository
+//2. Clone: each teammate clones on copy~
+//3. git checkout -b branchName --> create a new branch
+//4. git add READ_ME
+//git commit -m "comment"
+//git push --set-upstream origin haina (This is to create a branch.No need to do "git push --set-upstream origin haina" any more, instead do a simple "git push")
+
+//use <source tree> / <gitkraken> to better understand things visually 
+
+
+
+//usually name branches for the work content like "header" or ticket
+
+
+//gitkraken
+//1- click "Branch" to create a new branch
+//2- type the new branch name
+//3- go to VS code to change the code
+//4. back to gitkraken to stage and comment 
+//5- save the commit
